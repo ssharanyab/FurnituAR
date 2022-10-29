1b20b948a1e1744fb3c76f1d442b856f7ea7da2a
ssharanyab <ssharanyab@gmail.com>
2022-10-29 20:34:25 +0530

    splash screen done

---
+++ app/src/main/java/com/ssharanyab/furnituar/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ssharanyab.furnituar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FurnituAR)
+        setContentView(R.layout.activity_main)
+    }
+}
